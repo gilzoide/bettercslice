4e06b3a1580aa20c4a7bece439bbf3225ef33de8
gilzoide <gilzoide@gmail.com>
2020-12-07 00:47:17 -0300

    Add initial implementation
    TODO: document, add more relevant unittests

---
+++ source/bettercslice.d
@@ -0,0 +1,139 @@
+/++
+ + TODO: doc
+ +/
+struct Slice(T)
+{
+    T* ptr;
+    size_t length;
+
+    // InputRange
+    @property ref inout(T) front() inout
+    {
+        return ptr[0];
+    }
+
+    void popFront()
+    {
+        ptr += 1;
+        length -= 1;
+    }
+
+    @property bool empty() const
+    {
+        return length == 0;
+    }
+
+    // ForwardRange
+    inout(Slice) save() inout
+    {
+        return this;
+    }
+
+    // BidirectionalRange
+    @property ref inout(T) back() inout
+    {
+        return ptr[length - 1];
+    }
+
+    void popBack()
+    {
+        length -= 1;
+    }
+
+    // RandomAccessFinite
+    ref inout(T) opIndex(size_t i) inout
+    {
+        return ptr[i];
+    }
+
+    inout(Slice) opIndex(const size_t[2] s) inout
+    {
+        typeof(return) newSlice = {
+            ptr: ptr + s[0],
+            length: s[1] - s[0],
+        };
+        return newSlice;
+    }
+
+    size_t opDollar(size_t pos : 0)() const
+    {
+        return length;
+    }
+
+    inout(Slice) opIndex() inout
+    {
+        return this;
+    }
+
+    size_t[2] opSlice(size_t pos : 0)(size_t from, size_t to)
+    {
+        return [from, to];
+    }
+
+    // InputAssignable
+    @property void front(const T value)
+    {
+        ptr[0] = value;
+    }
+
+    // BidirectionalAssignable
+    @property void back(const T value)
+    {
+        ptr[length - 1] = value;
+    }
+
+    // RandomFiniteAssignable
+    void opIndexAssign(const T value, size_t index)
+    {
+        ptr[index] = value;
+    }
+}
+
+unittest
+{
+    import std.range.primitives;
+
+    alias IntSlice = Slice!int;
+    assert(isInputRange!IntSlice);
+    assert(isForwardRange!IntSlice);
+    assert(isBidirectionalRange!IntSlice);
+    assert(isRandomAccessRange!IntSlice);
+
+    assert(hasMobileElements!IntSlice);
+    assert(is(ElementType!IntSlice == int));
+    assert(hasSwappableElements!IntSlice);
+    assert(hasAssignableElements!IntSlice);
+    assert(hasLvalueElements!IntSlice);
+    assert(hasLength!IntSlice);
+    assert(!isInfinite!IntSlice);
+    assert(hasSlicing!IntSlice);
+}
+
+unittest
+{
+    import std.algorithm;
+    import std.stdio;
+    import std.range;
+
+    int[5] array = [1, 2, 3, 4, 5];
+
+    auto s = Slice!int(array.ptr, array.length);
+    const s2 = s;
+    writeln(s2[0]);
+    foreach (i; s)
+    {
+        writeln(i);
+    }
+    writeln("");
+    foreach (i; s.retro)
+    {
+        writeln(i);
+    }
+
+    writeln(s.stride(3));
+
+    writeln("");
+    s.filter!(x => x % 2 == 1).writeln;
+
+    alias StringSlice = Slice!string;
+}
